120f13278db6b826737cb33c2cc62fded553dd4c
danik <danik@DESKTOP-AIHBFQV>
2023-05-02 12:25:34 +0300

    e

--- L3/func.c
+++ L3/func.c
@@ -88,3 +88,109 @@ void apply_median_filter(RGBTRIPLE* pixels, int width, int height, int row_size,
     }
     free(temp_pixels);
 }
+int read_image(const char* input_file_name, BITMAPFILEHEADER* file_header, BITMAPINFOHEADER* info_header, RGBTRIPLE** pixels, int* row_size) {
+    FILE* input_file = fopen(input_file_name, "rb");
+    if (input_file == NULL) {
+        printf("\033[0;33m Error: could not open file %s\n \033[0m", input_file_name);
+        return -1;
+    }
+    fread(file_header, sizeof(BITMAPFILEHEADER), 1, input_file);
+    if (file_header->bfType != 0x4D42) {
+        printf("\033[0;33m Error: file %s is not a BMP file\n \033[0m", input_file_name);
+        fclose(input_file);
+        return -1;
+    }
+    fread(info_header, sizeof(BITMAPINFOHEADER), 1, input_file);
+    if (info_header->biBitCount != 24) {
+        printf("\033[0;33m Error: only support for processing of 24-bit images\n \033[0m");
+        fclose(input_file);
+        return -1;
+    }
+    *row_size = (info_header->biWidth * 3 + 3) & ~3;
+    *pixels = malloc(*row_size * info_header->biHeight);
+    fseek(input_file, file_header->bfOffBits, SEEK_SET);
+    for (int i = 0; i < info_header->biHeight; i++) {
+        fread(*pixels + *row_size / sizeof(RGBTRIPLE) * i, *row_size, 1, input_file);
+    }
+    fclose(input_file);
+    return 0;
+}
+
+int present_menu() {
+    int choice;
+    printf("\033[0;37m Select an operation:\n");
+    printf("1. Negative\n");
+    printf("2. Black and White\n");
+    printf("3. Apply multiple effects\n");
+    printf("4. Gamma Correction\n");
+    printf("5. Median Filter\n");
+    printf("6. Exit\n \033[0m");
+    printf("\033[0;32m Enter your choice: \033[0m");
+    scanf("%d", &choice);
+    return choice;
+}
+
+void process_choice(int choice, BITMAPFILEHEADER file_header, BITMAPINFOHEADER info_header, RGBTRIPLE* pixels, RGBTRIPLE* original_pixels, int row_size) {
+    char output_file_name[256];
+    printf("\033[0;32m Enter the name of the output file: \033[0m");
+    scanf("%s", output_file_name);
+    switch (choice) {
+    case 1:
+        apply_negative(pixels, info_header.biWidth, info_header.biHeight, row_size);
+        save_image(output_file_name, file_header, info_header, pixels, row_size);
+        memcpy(pixels, original_pixels, row_size * info_header.biHeight);
+        break;
+    case 2:
+        apply_black_and_white(pixels, info_header.biWidth, info_header.biHeight, row_size);
+        save_image(output_file_name, file_header, info_header, pixels, row_size);
+        memcpy(pixels, original_pixels, row_size * info_header.biHeight);
+        break;
+    case 3:
+        printf("\033[0;32m Enter the number of effects to apply: \033[0m");
+        int num_effects;
+        scanf("%d", &num_effects);
+        for (int i = 0; i < num_effects; i++) {
+            printf("\033[0;37m Select an effect to apply:\n");
+            printf("1. Negative\n");
+            printf("2. Black and White\n");
+            printf("3. Gamma Correction\n");
+            printf("4. Median Filter\n \033[0m");
+            printf("\033[0;32m Enter your choice: \033[0m");
+            int effect_choice;
+            scanf("%d", &effect_choice);
+            switch (effect_choice) {
+            case 1:
+                apply_negative(pixels, info_header.biWidth, info_header.biHeight, row_size);
+                break;
+            case 2:
+                apply_black_and_white(pixels, info_header.biWidth, info_header.biHeight, row_size);
+                break;
+            case 3:
+                printf("\033[0;32m Enter the value for gamma correction: \033[0m");
+                float gamma;
+                scanf("%f", &gamma);
+                while (gamma <= 0) {
+                    printf("\033[0;33m Error: gamma value must be greater than 0\n \033[0m");
+                    printf("\033[0;32m Enter the value for gamma correction: \033[0m");
+                    scanf("%f", &gamma);
+                }
+                apply_gamma_correction(pixels, info_header.biWidth, info_header.biHeight, row_size, gamma);
+                break;
+            case 4:
+                printf("\033[0;32m Enter the size of the median filter: \033[0m");
+                int filter_size;
+                scanf("%d", &filter_size);
+                while (filter_size < 3 || filter_size > 19) {
+                    printf("\033[0;33m Error: filter size must be between 3 and 19\n \033[0m");
+                    printf("\033[0;32m Enter the size of the median filter: \033[0m");
+                    scanf("%d", &filter_size);
+                }
+                apply_median_filter(pixels, info_header.biWidth, info_header.biHeight, row_size, filter_size);
+                break;
+            default:
+                printf("\033[0;33m Invalid choice\n \033[0m");
+                break;
+            }
+        }
+    }
+}
--- L3/func.h
+++ L3/func.h
@@ -40,4 +40,5 @@ void save_image(const char* file_name, BITMAPFILEHEADER file_header, BITMAPINFOH
 void apply_gamma_correction(RGBTRIPLE* pixels, int width, int height, int row_size, float gamma);
 int compare(const void* a, const void* b);
 void apply_median_filter(RGBTRIPLE* pixels, int width, int height, int row_size, int filter_size);
+int read_image(const char* input_file_name, BITMAPFILEHEADER* file_header, BITMAPINFOHEADER* info_header, RGBTRIPLE** pixels, int* row_size);
 #endif //INC_5_1_FUNCTIONS_H
